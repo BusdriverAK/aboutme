a4af9bc0a478cd96716c3a1bb6c7d4c1831d91d7
Mike K <m.kermes89@gmail.com>
2018-04-03 14:59:22 -0700

    started commenting out psuedo code / plans for js source file

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+confirm('Are you ready to play the guessing game?');
+//questions to ask for the code
+// How old am I
+/* run loop until user get's correct age. 3 outcomes correct, under, and over */
+// 
