a1da1ea7714f1b70ba2592933b43e3f3cdc81e3d
Mike K <m.kermes89@gmail.com>
2018-04-03 15:11:01 -0700

    finished commenting out questions that js will ask

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 confirm('Are you ready to play the guessing game?');
 //questions to ask for the code
-// How old am I
+// 1. How old am I
 /* run loop until user get's correct age. 3 outcomes correct, under, and over */
-// 
+// 2. do i like snowboarding more than skiing y/n
+// 3. is celery the one food i hate y/n
+// 4. Have i visited more than 10 countries y/n
+// 5. am i from seattle y/n
+// 6. Do you like celery? y/n
