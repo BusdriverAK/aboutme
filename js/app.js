0dffd37ad7b28036a77f64597e548a8f806d2a75
Mike K <m.kermes89@gmail.com>
2018-04-04 05:52:49 -0700

    added console logs for all user inputs

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ confirm('Are you ready to play the guessing game?');
 /* run loop until user get's correct age. 3 outcomes correct, under, and over */
 var myAge = 28;
 var guessAge = prompt('How old am I? Hint: 20-35');
+  console.log(guessAge);
 var intAge = parseInt(guessAge);
 
 if(intAge === myAge){
@@ -18,6 +19,7 @@ if(intAge === myAge){
 
 // 2. do i like snowboarding more than skiing y/n
 var downhillRec = prompt('Do I like snowboarding more than skiing? Yes or No?').toLowerCase();
+  console.log(downhillRec);
   if(downhillRec === 'no'){
     alert('You\'re correct! I enjoy both, but prefer skiing!');
   } else if(downhillRec === 'yes'){
@@ -29,6 +31,7 @@ var downhillRec = prompt('Do I like snowboarding more than skiing? Yes or No?').
 
 // 3. is celery the one food i hate y/n
 var ewwCelery = prompt('Is celery the worst food on the planet? Yes or No').toLowerCase();
+  console.log(ewwCelery);
   if(ewwCelery === 'yes'){
     alert('I wholeheartedly agree. And you are correct!');
   } else if(ewwCelery === 'no'){
@@ -39,6 +42,7 @@ var ewwCelery = prompt('Is celery the worst food on the planet? Yes or No').toLo
 
 // 4. Have i visited more than 10 countries y/n
 var visitNum = prompt('Have I visited more than 10 countries? Yes or No only.').toLowerCase();
+  console.log(visitNum);
   if(visitNum === 'yes'){
     alert('Correct! I can show you the world, Shining, shimmering splendid!');
   } else if (visitNum === 'no'){
@@ -50,6 +54,7 @@ var visitNum = prompt('Have I visited more than 10 countries? Yes or No only.').
 
 // 5. am i from seattle y/n
 var fromSeattle = prompt('Am I from Seattle? Yes or no.').toLowerCase();
+  console.log(fromSeattle);
   if(fromSeattle === 'no'){
     alert('That\'s Right! Born and raised in Alaska');
   } else if(fromSeattle === 'yes'){
@@ -60,6 +65,7 @@ var fromSeattle = prompt('Am I from Seattle? Yes or no.').toLowerCase();
 
 // 6. have i seen over 100 musical artists perform in a single year?
 var concertCount = prompt('Have I seen over 100 musical artists perform in a single year? Yes or No?').toLowerCase();
+  console.log(concertCount);
   if(concertCount === 'yes'){
     alert('You guessed it! What a year it was!');
   } else if(concertCount === 'no'){
